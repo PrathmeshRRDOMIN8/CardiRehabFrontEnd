ce8945763fc46e527ae6e6390f33322b2bb22816
ankitmishra1201 <workwithankit12@gmail.com>
2022-09-30 10:05:07 +0530

    Notification updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kmc_medical_app/UI/introScreen.dart';
 
@@ -19,8 +20,41 @@ import 'package:kmc_medical_app/Cardiac/controlBloodPressure.dart';
 import 'package:kmc_medical_app/Cardiac/controllingCholesterol.dart';
 import 'package:kmc_medical_app/Cardiac/bloodSugarLevel.dart';
 import 'package:kmc_medical_app/Cardiac/pyschologicalCare.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A bg message just showed up :  ${message.messageId}');
+}
+
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   runApp(MyApp());
 }
 
@@ -31,33 +65,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: IntroScreen.id,
-      routes: {
-        IntroScreen.id: (context) => IntroScreen(),
-        // DetailDivision.id : (context) => DetailDivision(),
-        ListRehab.id: (context) => ListRehab(),
-        // PushQuizPage.id : (context) => PushQuizPage(),
-        CareOfInciRite.id: (context) => CareOfInciRite(),
-        WoundCare.id: (context) => WoundCare(),
-        Bathing.id: (context) => Bathing(),
-        PhysicalActivity.id: (context) => PhysicalActivity(),
-        Rest.id: (context) => Rest(),
-        StairsClimbing.id: (context) => StairsClimbing(),
-        Posture.id: (context) => Posture(),
-        Medications.id: (context) => Medications(),
-        Exercise.id: (context) => Exercise(),
-        DietForCABG.id: (context) => DietForCABG(),
-        Smoking.id: (context) => Smoking(),
-        ControlBloodPressure.id: (context) => ControlBloodPressure(),
-        ControllingCholesterol.id: (context) => ControllingCholesterol(),
-        BloodSugarLevel.id: (context) => BloodSugarLevel(),
-        PsychologicalCare.id: (context) => PsychologicalCare(),
-      },
-    );
+  void initState() {
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text(notification.title!),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text(notification.body!)],
+                  ),
+                ),
+              );
+            });
+      }
+    });
   }
-}
+
+
+    // This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: IntroScreen.id,
+        routes: {
+          IntroScreen.id: (context) => IntroScreen(),
+          // DetailDivision.id : (context) => DetailDivision(),
+          ListRehab.id: (context) => ListRehab(),
+          // PushQuizPage.id : (context) => PushQuizPage(),
+          CareOfInciRite.id: (context) => CareOfInciRite(),
+          WoundCare.id: (context) => WoundCare(),
+          Bathing.id: (context) => Bathing(),
+          PhysicalActivity.id: (context) => PhysicalActivity(),
+          Rest.id: (context) => Rest(),
+          StairsClimbing.id: (context) => StairsClimbing(),
+          Posture.id: (context) => Posture(),
+          Medications.id: (context) => Medications(),
+          Exercise.id: (context) => Exercise(),
+          DietForCABG.id: (context) => DietForCABG(),
+          Smoking.id: (context) => Smoking(),
+          ControlBloodPressure.id: (context) => ControlBloodPressure(),
+          ControllingCholesterol.id: (context) => ControllingCholesterol(),
+          BloodSugarLevel.id: (context) => BloodSugarLevel(),
+          PsychologicalCare.id: (context) => PsychologicalCare(),
+        },
+      );
+    }
+  }
